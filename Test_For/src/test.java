b370a5273ff62e8ec85d3600ddd75532b083aa04
wyl <18395585578@163.com>
2020-09-04 20:52:24 +0800

    开发人员第④次修改test.java，先pull防止代码冲突

--- Test_For/src/test.java
+++ Test_For/src/test.java
@@ -11,5 +11,6 @@ public class test {
         System.out.println("修改");
         System.out.println("项目经理。。。");
         System.out.println("项目经理01");
+        System.out.println("开发人员先pull保持版本一致，然后再修改，防止代码冲突");
     }
 }
