d75d90de333bacc7490bf6db56da5ff47f0078e2
wyl <18395585578@163.com>
2020-09-05 09:40:43 +0800

    开发人员实验室添加Test

--- HomeTest/src/Hometest.java
+++ HomeTest/src/Hometest.java
@@ -1,8 +1,11 @@
+import org.junit.Test;
+
 public class Hometest {
     public static void main(String[] args) {
         System.out.println("Home test");
         System.out.println("Test02");
     }
+    @Test
     public void m1(){
         System.out.println("m1 方法");
     }
