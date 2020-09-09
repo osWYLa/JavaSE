6adee216dbeeb7fd9a371c1de9ddaad66bbe346b
wyl <18395585578@163.com>
2020-09-09 08:29:51 +0800

    properties (问题：properties如何控制重复键值对)

--- Collection_Map/src/Map/HashTable/PropertiesWrite.java
+++ Collection_Map/src/Map/HashTable/PropertiesWrite.java
@@ -6,29 +6,35 @@ import java.util.Properties;
 
 /**
  * @author wyl
- * @create 2020-09-08
+ * @create 2020-09-09
  * @Description
  * @Version
  */
-public class PropertiesTest {
+public class PropertiesWrite {
     public static void main(String[] args) {
         //创建一个Properties对象，按照key-value来进行管理
         Properties properties = new Properties();
+        FileOutputStream out = null;
         InputStream in = null;
         try {
-             in = new BufferedInputStream(new FileInputStream("E:\\\\IDEA_Project//new//////JavaSETest\\Collection_Map\\src\\Map\\HashTable\\my.properties"));
-            properties.load(in);//加载属性列表
+            out = new FileOutputStream("E:\\\\IDEA_Project//new//////JavaSETest\\Collection_Map\\src\\Map\\HashTable\\my.properties",true);
+            properties.setProperty("phone","10086");//增加
+            properties.store(out,"The new add message...."); //添加注释，为空也可，还用时间注释
+
+
+
+            in = new BufferedInputStream(new FileInputStream("E:\\\\IDEA_Project//new//////JavaSETest\\Collection_Map\\src\\Map\\HashTable\\my.properties"));
+            properties.load(in);
             Iterator<String> iterator = properties.stringPropertyNames().iterator();
             while (iterator.hasNext()){
                 String key = iterator.next();
-                System.out.println(key+":"+properties.getProperty(key));
+                System.out.println(key+":"+properties.get(key));
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                in.close();
+                out.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
